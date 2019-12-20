142e173cc64b33d6c97361f3e62e654167cc0831
harley garcia <hagarcia99@outlook.com>
2019-12-19 19:37:02 -0600

    string modify

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,7 +6,15 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Learning C#");
+            string myCar = "My new Honda Civic is white with black rims.";
+
+            Console.WriteLine(myCar.Substring(7, 5) );
+            Console.WriteLine(myCar[3]);
+            Console.WriteLine(myCar.Length);
+            Console.WriteLine(myCar.ToUpper() );
+            Console.WriteLine(myCar.ToLower() );
+
+            Console.ReadLine();
         }
     }
 }
