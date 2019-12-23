4ce3384730e2ce89a8d3fc620960bae9f84f29e6
harley garcia <hagarcia99@outlook.com>
2019-12-23 11:44:18 -0600

    Basic calc

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,12 +6,14 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " you are " + age + " years old");
 
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble( Console.ReadLine() );
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.WriteLine(num1 + num2);
+            
             Console.ReadLine();
         }
     }
