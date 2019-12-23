b79907a5e809a79538a2e009c3cc229993014bf7
harley garcia <hagarcia99@outlook.com>
2019-12-22 18:51:12 -0600

    Math methods

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,13 +6,12 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            string myCar = "My new Honda Civic is white with black rims.";
-
-            Console.WriteLine(myCar.Substring(7, 5) );
-            Console.WriteLine(myCar[3]);
-            Console.WriteLine(myCar.Length);
-            Console.WriteLine(myCar.ToUpper() );
-            Console.WriteLine(myCar.ToLower() );
+            Console.WriteLine( Math.Abs(-23) );
+            Console.WriteLine( Math.Sqrt(36) );
+            Console.WriteLine( Math.Pow(4, 2) );
+            Console.WriteLine( Math.Max(81, 60) );
+            Console.WriteLine( Math.Min(32, 53) );
+            Console.WriteLine( Math.Round(5.74) );
 
             Console.ReadLine();
         }
