5227aefed1974ac10d4a2ff507a7e6fc610efb2d
harley garcia <hagarcia99@outlook.com>
2019-12-23 12:15:15 -0600

    Basic mad libs

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,14 +6,22 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            string color, pluralNoun, food;
 
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble( Console.ReadLine() );
-            Console.Write("Enter another number: ");
-            double num2 = Convert.ToDouble( Console.ReadLine() );
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
 
-            Console.WriteLine(num1 + num2);
-            
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a food: ");
+            food = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + food);
+
+
             Console.ReadLine();
         }
     }
