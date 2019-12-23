d541fdad13e2655b59ef3135f1e908bd6c4ca427
harley garcia <hagarcia99@outlook.com>
2019-12-23 11:33:13 -0600

    User input

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,12 +6,11 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( Math.Abs(-23) );
-            Console.WriteLine( Math.Sqrt(36) );
-            Console.WriteLine( Math.Pow(4, 2) );
-            Console.WriteLine( Math.Max(81, 60) );
-            Console.WriteLine( Math.Min(32, 53) );
-            Console.WriteLine( Math.Round(5.74) );
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
 
             Console.ReadLine();
         }
