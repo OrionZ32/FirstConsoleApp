ab3263feba8423ba2b6343d8be07cff652045f9e
harley garcia <hagarcia99@outlook.com>
2019-12-25 14:49:50 -0600

    Returning data

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -9,12 +9,21 @@ namespace FirstConsoleApp
             int cubedNumber = cube(3);
             Console.WriteLine(cubedNumber);
 
+            int squaredNumber = square(4);
+            Console.WriteLine(squaredNumber);
+
             Console.ReadLine();
         }
 
         static int cube(int num)
         {
             int result = num * num * num;
+            return result;
+        }
+
+        static int square(int num)
+        {
+            int result = num * num;
             return result;
         }
        
