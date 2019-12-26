e5770c60d5e8e2c193535b6eaec5eb80cd22e1dc
harley garcia <hagarcia99@outlook.com>
2019-12-26 14:21:54 -0600

    Else if statements

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,14 +6,22 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            bool isMale = true;
+            bool isMale = false;
+            bool isTall = false;
 
-            if (isMale)
+            // && = both have to be true, || = only one has to be true
+            if (isMale && isTall)
             {
-                Console.WriteLine("You are male");
-            } else
+                Console.WriteLine("You are a tall male");
+            } else if (isMale && !isTall) 
             {
-                Console.WriteLine("You are not male");
+                Console.WriteLine("You are a short male");
+            } else if (!isMale && isTall) 
+            {
+                Console.WriteLine("You are not male but you wear mens size 13 nikes");            
+            } else 
+            {
+                Console.WriteLine("You are not male or tall");
             }
 
             Console.ReadLine();
