bedbf22267a44e2683b716d73a4be965f5497cf5
harley garcia <hagarcia99@outlook.com>
2019-12-27 07:29:44 -0600

    If comparisons

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,26 +6,30 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            bool isMale = false;
-            bool isTall = false;
-
-            // && = both have to be true, || = only one has to be true
-            if (isMale && isTall)
-            {
-                Console.WriteLine("You are a tall male");
-            } else if (isMale && !isTall) 
-            {
-                Console.WriteLine("You are a short male");
-            } else if (!isMale && isTall) 
-            {
-                Console.WriteLine("You are not male but you wear mens size 13 nikes");            
-            } else 
-            {
-                Console.WriteLine("You are not male or tall");
-            }
-
+            Console.WriteLine(GetMax(302, 112, 53));        
             Console.ReadLine();
         }
        
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else 
+            {
+                result = num3;
+            }
+
+
+            return result;
+        }
+
     }
 }
