b0c11bfc693633009bd529e66108f1acd8a8408d
harley garcia <hagarcia99@outlook.com>
2019-12-27 09:14:46 -0600

    Basic while loop

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,48 +6,14 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a day number between 0-6: ");
-            string userInput = Console.ReadLine();
-            Int32.TryParse(userInput, out int number);
-
-            Console.WriteLine(GetDay(number));
+            int index = 1;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            
             Console.ReadLine();
         }
-
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch (dayNum)
-            {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid day number";
-                    break;
-            }
-
-            return dayName;
-        }
-
     }
 }
