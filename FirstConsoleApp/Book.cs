6ba7c147c11b14f8df55eb7912d1e21cb14cf17a
harley garcia <hagarcia99@outlook.com>
2020-01-02 11:47:31 -0600

    Classes and objects

---
+++ FirstConsoleApp/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleApp
+{
+    class Book
+    {
+        public string title;
+        public string authour;
+        public int pages;
+    }
+}
--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,24 +6,23 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            Book book1 = new Book();
+            book1.title = "Fahrenheit 451";
+            book1.authour = "Ray Bradbury";
+            book1.pages = 158;
 
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+            Book book2 = new Book();
+            book2.title = "The Giver";
+            book2.authour = "Lois Lowry";
+            book2.pages = 240;
 
-                Console.WriteLine(num1 + " divided by " + num2 + " equals: " + num1 / num2);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message;
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }            
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.authour);
+            Console.WriteLine(book1.pages);
+            Console.WriteLine();
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.authour);
+            Console.WriteLine(book2.pages);
 
             Console.ReadLine();
         }
