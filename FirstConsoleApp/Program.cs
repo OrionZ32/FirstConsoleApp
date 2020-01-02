d6bac1f1e4604b26b56b253f0996c5e90e5e0f3d
harley garcia <hagarcia99@outlook.com>
2020-01-02 11:24:53 -0600

    Exception handling

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,13 +6,24 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            int[,] numberGrid = {
-                {1, 2},
-                {3, 4},
-                {5, 6}
-            };
 
-            Console.WriteLine(numberGrid[1, 1]);
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 + " divided by " + num2 + " equals: " + num1 / num2);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }            
 
             Console.ReadLine();
         }
