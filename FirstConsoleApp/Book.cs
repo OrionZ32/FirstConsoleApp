3b7c6e88ad06cc52fcb46848b371dc4fa784ea88
harley garcia <hagarcia99@outlook.com>
2020-01-02 12:45:50 -0600

    Constructors

--- FirstConsoleApp/Book.cs
+++ FirstConsoleApp/Book.cs
@@ -7,7 +7,20 @@ namespace FirstConsoleApp
     class Book
     {
         public string title;
-        public string authour;
+        public string author;
         public int pages;
+
+        public Book()
+        {
+
+        }
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
     }
 }
--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,24 +6,14 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.title = "Fahrenheit 451";
-            book1.authour = "Ray Bradbury";
-            book1.pages = 158;
-
-            Book book2 = new Book();
-            book2.title = "The Giver";
-            book2.authour = "Lois Lowry";
-            book2.pages = 240;
+            Book book1 = new Book("Fahrenheit 451", "Ray Bradbury", 158);
+            Book book2 = new Book("The Giver", "Lois Lowry", 240);
+            Book book3 = new Book();
 
             Console.WriteLine(book1.title);
-            Console.WriteLine(book1.authour);
+            Console.WriteLine(book1.author);
             Console.WriteLine(book1.pages);
-            Console.WriteLine();
-            Console.WriteLine(book2.title);
-            Console.WriteLine(book2.authour);
-            Console.WriteLine(book2.pages);
-
+            
             Console.ReadLine();
         }
 
